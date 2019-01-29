6e9a30eceb12b36562c3104b9d87b5b8737547e0
Jesse Zhang <sbjesse@gmail.com>
2019-01-28 16:19:47 -0800

    pending signals. Pair: Goutam

--- signals/ex.pending_sig_expt.c
+++ signals/ex.pending_sig_expt.c
@@ -43,24 +43,85 @@
 /* FIXME: Implement handlers for SIGINT and SIGQUIT; the latter handler
    should display a message that SIGQUIT has been caught */
 
+static void block_every_sig_except_int()
+{
+    sigset_t mask;
+    sigfillset(&mask);
+    sigdelset(&mask, SIGINT);
+    sigprocmask(SIG_SETMASK, &mask, NULL);
+}
+
+static void unblock_sigquit()
+{
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGQUIT);
+    sigprocmask(SIG_UNBLOCK, &mask, NULL);
+}
+
+static void handle_int(int _)
+{
+    return;
+}
+
+static void handle_quit(int _)
+{
+    printf("SIGQUIT received\n"); /* UNSAFE?? */
+}
+
+static void print_all_pending_sigs()
+{
+    sigset_t mask;
+    int ret = sigpending(&mask);
+    if (ret != 0)
+        errExit("sigpending");
+    for (int i = 1; i < NSIG; ++i)
+    {
+        int yolo = sigismember(&mask, i);
+        switch (yolo)
+        {
+            case 0:
+                continue;
+            case 1:
+                break;
+            default:
+                errExit("sigismember returns %d for signal %d", yolo, i);
+        }
+        printf("pending signal: %s\n", strsignal(i));
+    }
+}
+
+static void setup_handler_for_int_and_quit()
+{
+    struct sigaction s = {
+            .sa_flags = 0,
+            .sa_handler = handle_int,
+    };
+    sigemptyset(&s.sa_mask);
+
+    sigaction(SIGINT, &s, NULL);
+
+    s.sa_handler = handle_quit;
+    sigaction(SIGQUIT, &s, NULL);
+}
+
 int
 main(int argc, char *argv[])
 {
 
     /* FIXME: Add variable declarations as required */
 
-    /* FIXME: Block all signals except SIGINT */
+    block_every_sig_except_int();
 
     /* FIXME: Set up handlers for SIGINT and SIGQUIT */
+    setup_handler_for_int_and_quit();
 
     /* Block until SIGINT handler is invoked */
 
     printf("Pausing... send me some signals (PID=%ld)\n", (long) getpid());
     pause();
-
-    /* FIXME: Retrieve and display set of pending signals */
-
-    /* FIXME: Unblock SIGQUIT */
+    print_all_pending_sigs();
+    unblock_sigquit();
 
     pause();
 
